39189d0885bbcbdeceade64b10bfa82b1354e685
gcdadich <97729024+gcdadich@users.noreply.github.com>
2023-04-15 23:14:19 +0530

    Update kgc.java

--- kgc.java
+++ kgc.java
@@ -5,6 +5,6 @@ public class kgc {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a+b;
-        System.out.println(sum);
+        System.out.println(sum);      //gcdadich
     }
 }
