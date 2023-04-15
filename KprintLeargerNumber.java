5515ff95ea46c778cef800c1ca4e6a165b43fa49
gcdadich <gulabchand995@gmail.com>
2023-04-16 01:05:13 +0530

    new

---
+++ KprintLeargerNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class KprintLeargerNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println("The learger Number is "+a);
+        }
+        else{
+            System.out.println("The learger Number is "+b);
+        }
+
+    }
+}
--- jgcone.java
+++
@@ -1,8 +0,0 @@
-public class jgcone {
-    public static void main(String[] args){
-        int a =1900;
-        int b = 2000;
-        System.out.println(a+b);
-    }
-    
-}
